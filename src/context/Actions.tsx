56c74da48becd930d1b36bfc5f2214bff703d359
tunji ty <tytunji29@gmail.com>
2025-06-24 18:52:36 +0100

    nnnnn

--- src/context/Actions.tsx
+++ src/context/Actions.tsx
@@ -4,7 +4,7 @@ import user from "./UserActions";
 import shipments from "./ShipmentActions";
 
 //export const APIURL = "https://vubids.online/app/v1";
-export const APIURL = "http://temitayo01-001-site1.ktempurl.com/app/v1";
+export const APIURL = "https://temitayo01-001-site1.ktempurl.com/app/v1";
 export const position = "bottom-center";
 export const vubids_user =
   typeof window !== "undefined"
